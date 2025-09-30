d1a3be8efbc70524ebba6df2da4467b94b00ca12
Deepak <deepakchoudhary2030@gmail.com>
2025-09-30 16:57:56 +0530

    Added code to handle negative numbers

--- app/services/string_calculator_service.rb
+++ app/services/string_calculator_service.rb
@@ -4,7 +4,30 @@ class StringCalculatorService
     if string.blank?
       0
     else
-      string.split(/,|\n|;/).map(&:to_i).sum
+      split_numbers = string.split(/,|\n|;/)
+
+      integer_numbers = []
+      split_numbers.each do |num_str|
+        integer_numbers << num_str.to_i
+      end
+
+      negative_numbers = []
+      integer_numbers.each do |num|
+        if num < 0
+          negative_numbers << num
+        end
+      end
+
+      if negative_numbers.length > 0
+        raise "Negative numbers are not allowed: #{negative_numbers.join(', ')}"
+      end
+
+      sum = 0
+      integer_numbers.each do |num|
+        sum += num
+      end
+
+      sum
     end
   end
 end
