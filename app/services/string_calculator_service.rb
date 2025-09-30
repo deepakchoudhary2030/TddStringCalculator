f6f96e4fb57568939b7e807388b5146a5bbf7823
Deepak <deepakchoudhary2030@gmail.com>
2025-09-30 15:33:44 +0530

    Added Rspec for empty string

--- app/services/string_calculator_service.rb
+++ app/services/string_calculator_service.rb
@@ -1,4 +1,6 @@
 # app/services/string_calculator_service.rb
 class StringCalculatorService
-
+  def addition(string)
+    0
+  end
 end
--- spec/services/string_calculator_service_spec.rb
+++ spec/services/string_calculator_service_spec.rb
@@ -1,5 +1,11 @@
 # spec/services/string_calculator_service_spec.rb
 require 'rails_helper'
 RSpec.describe StringCalculatorService do
+  let(:string_calculator) { described_class.new }
 
+  describe "addition" do
+    it 'will return zero if the string is empty' do
+      expect(string_calculator.addition('')).to eq(0)
+    end
+  end
 end
